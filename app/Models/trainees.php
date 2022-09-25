d29ea056119657cc27644f5d52d42129d94b4a33
FRANZSAIJEM <mikoremixes@gmail.com>
2022-09-25 08:49:27 +0800

    initial

--- app/Models/trainees.php
+++ app/Models/trainees.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class trainees extends Model
 {
     use HasFactory;
+
+    public function courses(){
+        return $this->belongsTo('app\Models\courses');
+    }
+
+    public function User(){
+        return $this->belongsTo('app\Models\User');
+    }
 }
