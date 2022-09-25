87403d28f519c4b9bc195e2411364750ebbb0e61
ParaDoxical12345 <Estillorelawrence7@gmail.com>
2022-09-25 08:05:36 +0800

    initial

--- database/migrations/2022_09_24_231323_create_trainors_table.php
+++ database/migrations/2022_09_24_231323_create_trainors_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('trainors', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('specialty');
+            $table->timestamps();
+
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -27,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('trainors');
     }
 };
